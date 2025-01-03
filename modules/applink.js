76c0014b3e4dad3eb29ae2d65ba51b172c35a907
ryota-mac <cool.sn.115@gmail.com>
2025-01-04 00:54:34 +0900

    テストアップ

---
+++ modules/applink.js
@@ -0,0 +1,13 @@
+export function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent;
+
+    if (userAgent.indexOf('Android') > 0) {
+        return "Android";
+    }
+
+    if (userAgent.indexOf('iPhone') > 0 || userAgent.indexOf('iPad') > 0 || userAgent.indexOf('iPod') > 0 || (userAgent.indexOf('Macintosh') > 0 && document.ontouchstart !== undefined)) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
